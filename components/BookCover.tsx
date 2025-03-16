7175df885667930784ee1ba6fdb12b66382593b8
Maycon Marques <mayconmarquesh@gmail.com>
2025-03-15 21:44:21 -0300

    Add BookCover, BookList, and BookOverview components

---
+++ components/BookCover.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import React from "react"
+
+type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide"
+
+type Props = {
+   className?: string
+   variant: BookCoverVariant
+   coverColor: string
+   coverUrl: string
+}
+
+const variantStyles: Record<BookCoverVariant, string> = {
+   extraSmall: "book-cover_extra_small",
+   small: "book-cover_small",
+   medium: "book-cover_medium",
+   regular: "book-cover_regular",
+   wide: "book-cover_wide"
+}
+
+export default function BookCover({ className, variant = "regular", coverColor = "#012b48", coverUrl = "https://placehold.co/400x600.png" }: Props) {
+   return (
+      <div className={cn("relative transition-all duration-300", variantStyles[variant], className)} style={{ backgroundColor: coverColor }}>
+         BOOK SVG
+         <div className="absolute z-10 left-[12%] w-[87.5%] h-[88%]">
+            <Image src={coverUrl} alt="Book cover" fill className="rounded-sm object-fill" />
+         </div>
+      </div>
+   )
+}
---
+++ components/BookList.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function BookList() {
+   return (
+      <section>
+         <h2 className="font-bebas-neue text-4xl text-light-100">Book List</h2>
+      </section>
+   )
+}
