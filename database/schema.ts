567c6437600a0d5016748c01e2704adc3d8c92ff
Maycon Marques <mayconmarquesh@gmail.com>
2025-04-22 23:24:11 -0300

    add drizzle+, setup with neon pg, schema and more

---
+++ database/schema.ts
@@ -0,0 +1,25 @@
+import { date, integer, pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+
+export const STATUS_ENUM = pgEnum("status", ["PENDING", "APPROVED", "REJECTED"])
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"])
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", ["BORROWED", "RETURNED"])
+
+export const users = pgTable("users", {
+   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
+   fullName: varchar("full_name", { length: 255 }).notNull(),
+   email: text("email").notNull().unique(),
+   universityId: integer("university_id").notNull().unique(),
+   password: text("password").notNull(),
+   univerityCard: text("university_card").notNull(),
+   status: STATUS_ENUM("status").default("PENDING").notNull(),
+   role: ROLE_ENUM("role").default("USER"),
+   lastActivityDate: date("last_activity_date").notNull().defaultNow(),
+
+   createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+   updatedAt: timestamp("updated_at", { withTimezone: true })
+      .notNull()
+      .$onUpdate(() => new Date())
+})
+
+export type InsertUser = typeof users.$inferInsert
+export type SelectUser = typeof users.$inferSelect
--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
    env: {
       apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3000",
+      databaseUrl: process.env.DATABASE_URL || "",
       imagekit: {
          publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
          urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_ENDPOINT || "",
