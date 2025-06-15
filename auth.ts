e1ea0b873af1af1615da5fa750f1353b5c8e2cd0
Maycon Marques <mayconmarquesh@gmail.com>
2025-06-15 11:51:15 -0300

    fix: error msgs no auth

--- auth.ts
+++ auth.ts
@@ -7,53 +7,53 @@ import { db } from "./database/drizzle"
 import { users } from "./database/schema"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-   session: {
-      strategy: "jwt"
-   },
-   providers: [
-      CredentialsProvider({
-         async authorize(credentials) {
-            if (!credentials?.email || !credentials?.password) {
-               return null
-            }
+  session: {
+    strategy: "jwt"
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email ou senha inválidos.")
+        }
 
-            const user = await db.select().from(users).where(eq(users.email, credentials.email.toString())).limit(1)
+        const user = await db.select().from(users).where(eq(users.email, credentials.email.toString())).limit(1)
 
-            if (user.length === 0) {
-               return null
-            }
+        if (user.length === 0) {
+          throw new Error("Email ou senha inválidos.")
+        }
 
-            const isPasswordValid = await compare(credentials.password.toString(), user[0].password)
+        const isPasswordValid = await compare(credentials.password.toString(), user[0].password)
 
-            if (!isPasswordValid) {
-               return null
-            }
+        if (!isPasswordValid) {
+          throw new Error("Email ou senha inválidos.")
+        }
 
-            return {
-               id: user[0].id.toString(),
-               name: user[0].fullName,
-               email: user[0].email
-            } as User
-         }
-      })
-   ],
-   pages: {
-      signIn: "/sign-in"
-   },
-   callbacks: {
-      async jwt({ token, user }) {
-         if (user) {
-            token.id = user.id
-            token.name = user.name
-         }
-         return token
-      },
-      async session({ session, token }) {
-         if (session.user) {
-            session.user.id = token.id as string
-            session.user.name = token.name as string
-         }
-         return session
+        return {
+          id: user[0].id.toString(),
+          name: user[0].fullName,
+          email: user[0].email
+        } as User
       }
-   }
+    })
+  ],
+  pages: {
+    signIn: "/sign-in"
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.name = user.name
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.name = token.name as string
+      }
+      return session
+    }
+  }
 })
